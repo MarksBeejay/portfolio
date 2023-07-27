fec2039addf9820cd221eea7361804d4f77cbe44
beejaymarkz <makanjuolabolaji9898@gmail.com>
2023-07-27 05:59:22 +0100

    server modified

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
-// const mongoose = require('mongoose');
+// const cors = require('cors');
+const mongoose = require('mongoose');
 const form = require('./src/routes/forms')
 
 // Connect to MongoDB
-// mongoose.Promise = global.Promise;
-// mongoose.connect(process.env.MONGODB_CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log('Connected to MongoDB'))
-//   .catch((error) => console.error('MongoDB connection error:', error));
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.error('MongoDB connection error:', error));
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'))
 app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
 // app.use('portfolio/contact', cors({
 //   origin: 'https://makanjuolabolaji.netlify.app/',
 //   methods: ['GET', 'POST'],
@@ -29,12 +29,12 @@ app.use(bodyParser.json());
 // Define your routes here
 app.use('/portfolio', form)
 
-// app.use((req, res, next)=>{
-//     const err = new Error('not found');
-//     err.status = 404;
-//     err.message = 'route not found';
-//     next(err)
-//   });
+app.use((req, res, next)=>{
+    const err = new Error('not found');
+    err.status = 404;
+    err.message = 'route not found';
+    next(err)
+  });
 
 // app.get('/', (req, res) => {
 //   res.send('https://makanjuolabolaji.netlify.app/');
